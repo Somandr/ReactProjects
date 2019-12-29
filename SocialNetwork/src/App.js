0ab10b8d5f4f6ac8d4638afda966c2d078d27370
Andrew Somin <4792953-Somandr@users.noreply.gitlab.com>
2019-12-29 19:25:30 +0200

    comment added - App.js

--- SocialNetwork/src/App.js
+++ SocialNetwork/src/App.js
@@ -1,32 +1,35 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
-import { BrowserRouter, Route } from "react-router-dom";
-
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Sidebar from './components/Sidebar/Sidebar';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
+// test
 const App = props => {
-  return (
-    <div className="app-wrapper d_grid">
-      <Header />
-      <Sidebar />
-      <div className="app-wrapper-content">
-        <Route
-          path="/profile"
-          render={() => (
-            <Profile
-               profilePage={props.state.profilePage}
-              dispatch={props.dispatch}
-            />
-          )}
-        />
-        <Route path="/dialogs" render={() => <Dialogs store={props.store} />} />
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="app-wrapper d_grid">
+            <Header />
+            <Sidebar />
+            <div className="app-wrapper-content">
+                <Route
+                    path="/profile"
+                    render={() => (
+                        <Profile
+                            profilePage={props.state.profilePage}
+                            dispatch={props.dispatch}
+                        />
+                    )}
+                />
+                <Route
+                    path="/dialogs"
+                    render={() => <Dialogs store={props.store} />}
+                />
+            </div>
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
