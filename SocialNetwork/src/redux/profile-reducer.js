2fdd3065c6eaf9a8fa8610ea5f8d1a1dc0b8c79d
Andrew Somin <4792953-Somandr@users.noreply.gitlab.com>
2019-12-30 03:47:14 +0200

    creation a copy of the state object in the profileReducer

--- SocialNetwork/src/redux/profile-reducer.js
+++ SocialNetwork/src/redux/profile-reducer.js
@@ -1,43 +1,46 @@
-const ADD_POST = "ADD-POST";
-const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 let initialState = {
-  postsData: [
-    { id: 1, message: "Hi, how are you?", likesCount: 11 },
-    { id: 2, message: "Hi, how are you?", likesCount: 9 },
-    { id: 3, message: "Hi, how are you?", likesCount: 7 },
-    { id: 4, message: "Hi, how are you?", likesCount: 15 },
-    { id: 5, message: "Hi, how are you?", likesCount: 21 }
-  ],
-  newPostText: "Uber"
-}
+    postsData: [
+        { id: 1, message: 'Hi, how are you?', likesCount: 11 },
+        { id: 2, message: 'Hi, how are you?', likesCount: 9 },
+        { id: 3, message: 'Hi, how are you?', likesCount: 7 },
+        { id: 4, message: 'Hi, how are you?', likesCount: 15 },
+        { id: 5, message: 'Hi, how are you?', likesCount: 21 },
+    ],
+    newPostText: 'Uber',
+};
 
 const profileReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_POST:
-      let newPost = {
-        id: 5,
-        message: state.newPostText,
-        likesCount: 1
-      };
-      state.postsData.push(newPost);
-      state.newPostText = "";
-      return state;
-    case UPDATE_NEW_POST_TEXT:
-      state.newPostText = action.newText;
-      return state;
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case ADD_POST: {
+            let newPost = {
+                id: 5,
+                message: state.newPostText,
+                // likesCount: 1,
+            };
+            let stateCopy = { ...state };
+            stateCopy.postsData = [...state.postsData];
+            stateCopy.postsData.push(newPost);
+            stateCopy.newPostText = '';
+            return stateCopy;
+        }
+        case UPDATE_NEW_POST_TEXT: {
+            let stateCopy = { ...state };
+            stateCopy.postsData = [...state.postsData];
+            stateCopy.newPostText = action.newText;
+            return stateCopy;
+        }
+        default:
+            return state;
+    }
 };
 
 export const addPostActionCreator = () => ({ type: ADD_POST });
 export const updateNewPostTextActionCreator = text => ({
-  type: UPDATE_NEW_POST_TEXT,
-  newText: text
+    type: UPDATE_NEW_POST_TEXT,
+    newText: text,
 });
-
-
-
 
 export default profileReducer;
